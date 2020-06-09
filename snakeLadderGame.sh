6fec04ddbe31a482ff1b7436bbb05239a5b16298
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-09 16:24:28 +0530

    check noplay ladder snake condition

--- snakeLadderGame.sh
+++ snakeLadderGame.sh
@@ -1,7 +1,46 @@
 #!/bin/bash -x
 echo "Welcome To Snake Ladder Game";
-
+#CONSTANTS
+NOPLAY=0;
+LADDERPLAY=1;
+SNAKEPLAY=2;
+STARTPOINT=0;
+ENDPOINT=100;
+#VARIABLES
 playerPosition=0;
-dieValue=$((RANDOM%6 + 1));
-echo $dieValue;
+#This Loop Runs Until Player position Not reached upto Endpoint
+	while :
+	do
+		dieValue=$((RANDOM%6 + 1));
+		playValue=$((RANDOM%3));
 
+		if [ $playValue -eq $NOPLAY ]
+		then
+			playerPosition=$playerPosition;
+
+		elif [ $playValue -eq $LADDERPLAY ]
+		then
+			#This Is Ladder Play
+			#checkEnd variable for to check ladder extends Max point value
+			checkEnd=$playerPosition;
+			playerPosition=$(( $playerPosition + $dieValue ));
+			if [ $playerPosition -gt $ENDPOINT ]
+			then
+				playerPosition=$checkEnd;
+			fi;
+
+		else
+			#This is Snake Play Here
+			playerPosition=$(( $playerPosition - $dieValue ));
+			if [ $playerPosition -le $STARTPOINT ]
+			then
+				playerPosition=$STARTPOINT;
+			fi;
+		fi;
+#Checking Player Position Reached or Not
+		if [ $playerPosition -eq $ENDPOINT ]
+		then
+			echo "Player Won The GAME";
+			break;
+		fi;
+	done
