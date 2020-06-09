338f5172a2dc88e9d448db47687fc43faaa4bb05
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-09 22:11:00 +0530

    Added Multiplayer Feature

--- snakeLadderGame.sh
+++ snakeLadderGame.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash -x
 echo "Welcome To Snake Ladder Game";
 #CONSTANTS
@@ -8,13 +7,31 @@ SNAKEPLAY=2;
 STARTPOINT=0;
 ENDPOINT=100;
 #VARIABLES
+player1Position=0;
+player2Position=0;
+#flag=1 means first start with  player1
+flag=1;
+#Laddercheck for to continue same Player
+ladderCheck=0;
 playerPosition=0;
 dieCounter=0;
 #Dictionary
-declare -A positionStore;
+declare -A player1Storage;
+declare -A player2Storage;
 #This Loop Runs Until Player position Not reached upto Endpoint
 	while :
 	do
+		#------This If for Multiplayer-----
+		if [ $flag -eq 1 ]
+		then
+			playerPosition=$player1Position;
+
+		else
+			playerPosition=$player2Position;
+		fi;
+		#------Multiplayer Code End Here----
+
+		# Dice Counter TO store How many Time We Rolled Die
 		((dieCounter++));
 		dieValue=$((RANDOM%6 + 1));
 		playValue=$((RANDOM%3));
@@ -32,6 +49,8 @@ declare -A positionStore;
 			playerPosition=$(( $playerPosition + $dieValue ));
 			if [ $playerPosition -gt $ENDPOINT ]
 			then
+				#LadderCheck to continue with Same Player
+				ladderCheck=1;
 				playerPosition=$checkEnd;
 			fi;
 
@@ -43,19 +62,72 @@ declare -A positionStore;
 				playerPosition=$STARTPOINT;
 			fi;
 		fi;
+		#-------This Code For multiplayer---------
 
-		#maintaining The Positon After Die Rolling
-		positionStore[ $playerPosition ]=$dieValue;
-		#Checking Player Position Reached or Not
-		if [ $playerPosition -eq $ENDPOINT ]
+		#I AM PLAYER1
+		if [ $flag -eq 1 ]
 		then
-			echo "Player Won The GAME";
-			echo "Dice Rolled count=" $dieCounter;
-			#Displayig position and dice Value;
-			for i in "${!positionStore[@]}"
-			do
-  			echo "Position  : $i"   "DICE Value: ${positionStore[$i]}";
-			done
+			#if ladder occured then same player Play again
+			if [ $ladderCheck -eq 1 ]
+			then
+				ladderCheck=0;
+				flag=1;
+			else
+				#Ladder Not Occured So Passing To other Player
+				ladderCheck=0;
+				flag=0;
+			fi;
+			#storing Player position
+			player1Position=$playerPosition;
+			player1Storage[ $playerPosition ]=$dieValue;
+
+      else
+
+			#I AM PLAYER 2
+			if [ $ladderCheck -eq 1 ]
+			then
+			ladderCheck=0;
+			flag=0;
+			else
+			ladderCheck=0;
+			flag=1;
+			fi;
+			#Storing Position
+         player2Position=$playerPosition;
+			player2Storage[ $playerPosition ]=$dieValue;
+	 	fi;
+		#-----Multiplayer Code Ended Here--------------
+
+		#FINAL Checking for Which player Reached 100 First
+		if [ $player1Position -eq $ENDPOINT ] || [ $player2Position -eq $ENDPOINT ] 
+		then
+			#According To winner We Displaying Records
+			if [ $player1Position -eq $ENDPOINT ]
+			then
+				echo "*****CONGRATS*****";
+				printf "\n \n";
+				echo "WINNER IS= PLAYER 1";
+				printf "\n \n";
+				echo "Dice Rolled count=" $dieCounter;
+				printf "\n \n";
+				for i in "${!player1Storage[@]}"
+         	do
+            	echo "Position  : $i"   "DICE Value: ${player1Storage[$i]}";
+         	done
+
+			else
+				echo "*****CONGRATS*****";
+				printf "\n \n";
+				echo "WINNER IS= PLAYER 2";
+				printf "\n \n";
+				echo "Dice Rolled count=" $dieCounter;
+				printf "\n \n";
+				for i in "${!player2Storage[@]}"
+         	do
+            	echo "Position  : $i"   "DICE Value: ${player2Storage[$i]}";
+         	done
+			fi;
+
 			break;
 		fi;
 	done
